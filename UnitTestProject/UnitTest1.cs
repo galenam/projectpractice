b7dc5da2982f23e7beafe728dd2d2ecaae7ab9f7
gmelnik <Galina.Melnik@homecredit.ru>
2017-08-17 10:05:52 +0300

    add 2 more test. need to add 1 neccessary  and some (2 or more) tests for boundary conditions

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -26,33 +26,74 @@ namespace UnitTestProject
 		}
 
 		// todo : разобраться с инициализацией значений (напр. initialVM) общим методом
+		// todo : что будет, если я внесу номинал, которого нет в кошельке, например, 11 рублей одним вызовом ?
+		// todo : что вернет метод Delete, если пытаться вернуть деньги из автомата  до внесения ?
 		[TestMethod]
 		public void TestMethodPut()
 		{
 			var initialVm = GetVM();
 			var nominal = 1;
-			initialVm.InsertedSum += nominal;
-			if (initialVm.UserCoins.ContainsKey(nominal))
-			{
-				initialVm.UserCoins[nominal]++;
-			}
-
+			
 			var controller = new HomeController(initialVm);
 			var result = controller.Put(nominal);
 			
 			var putAnonimusResult = new PrivateObject(result);
 			var content = new PrivateObject(putAnonimusResult.GetFieldOrProperty("Content"));
 			var success = (bool)content.GetProperty("Success");
+			Assert.AreEqual(success, true, "Operation insert failed");
+
 			var insertedSum = (int)content.GetProperty("InsertedSum");
 			var userCoins = (System.Collections.Generic.SortedDictionary<int, int>)content.GetProperty("UserCoins");
 			var machineCoins = (System.Collections.Generic.SortedDictionary<int, int>)content.GetProperty("MachineCoins");
 
-			
-			//var tmp = new VendingMachine(initialVm.Beverages, );
+			var vendingMachineWithCoins = new VendingMachine(initialVm.Beverages, userCoins , machineCoins){InsertedSum = insertedSum, State = new InsertedUserCoinsState()};
 
+			var testVendingMachine = GetVM();
+			testVendingMachine.InsertedSum += nominal;
+			if (testVendingMachine.UserCoins.ContainsKey(nominal))
+			{
+				testVendingMachine.UserCoins[nominal]++;
+			}
 
+			Assert.AreEqual(vendingMachineWithCoins, initialVm);
+		}
 
+		[TestMethod]
+		public void TestMethodDelete()
+		{
+			var initialVm = GetVM();
 
+			// клиент накидал 56 р 1-рублевыми монетами
+			var controller = new HomeController(initialVm);
+			for (var i = 0; i++ < 56;)
+			{
+				controller.Put(1);
+			}
+
+			var result = controller.Delete();
+
+			var putAnonimusResult = new PrivateObject(result);
+			var content = new PrivateObject(putAnonimusResult.GetFieldOrProperty("Content"));
+			var success = (bool)content.GetProperty("Success");
+			Assert.AreEqual(success, true, "Operation Delete failed");
+
+			var insertedSum = (int)content.GetProperty("InsertedSum");
+			var userCoins = (System.Collections.Generic.SortedDictionary<int, int>)content.GetProperty("UserCoins");
+			var machineCoins = (System.Collections.Generic.SortedDictionary<int, int>)content.GetProperty("MachineCoins");
+
+			var vendingMachineWithCoins = new VendingMachine(initialVm.Beverages, userCoins, machineCoins) { InsertedSum = insertedSum, State = new NoUserCoinsState() };
+
+			// автомат должен вернуть максимально крупными
+			var testVendingMachine = GetVM();
+			testVendingMachine.UserCoins[5]-=11;
+			testVendingMachine.UserCoins[1]-- ;
+
+			Assert.AreEqual(vendingMachineWithCoins, initialVm);
+		}
+
+		[TestMethod]
+		public void TestMethodPost()
+		{
 
 		}
 
